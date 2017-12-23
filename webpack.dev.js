37109586806076869a4529fa2ace62bccb30fdcc
shahnawaz19415 <shahnawaz19415@gmail.com>
2017-12-23 20:48:49 +0500

    Webpack, babelrc and package.json configured

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack'),
+      merge = require('webpack-merge'),
+      common = require('./webpack.common.js'),
+      path = require('path');
+
+ module.exports = merge(common, {
+   devtool: 'inline-source-map',
+   devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3000
+   }
+ });
